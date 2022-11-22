c5370534eda00681ab828ba7153e87f0d561e238
abdulkadirerdem <akadir_1256@hotmail.com>
2022-11-22 17:47:31 +0300

    Screens Created

---
+++ src/screens/UpdateTodoScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const UpdateTodoScreen = () => {
+  return (
+    <View>
+      <Text>UpdateTodoScreen</Text>
+    </View>
+  );
+};
+
+export default UpdateTodoScreen;
+
+const styles = StyleSheet.create({});
