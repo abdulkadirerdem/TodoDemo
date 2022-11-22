c1c546a8168e3861989467d9a44fa06eedec45d3
abdulkadirerdem <akadir_1256@hotmail.com>
2022-11-22 18:24:47 +0300

    UI Kitten Library Added

--- App.js
+++ App.js
@@ -2,6 +2,10 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
+// UI Kitten
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
+
 // Screens
 import LoginScreen from "./src/screens/LoginScreen";
 import HomeScreen from "./src/screens/HomeScreen";
@@ -10,22 +14,24 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          options={{ headerShown: false, statusBarHidden: true }}
-          name="Login"
-          component={LoginScreen}
-        />
-        <Stack.Screen
-          options={{
-            headerShown: false,
-            statusBarHidden: true,
-          }}
-          name="Home"
-          component={HomeScreen}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            options={{ headerShown: false, statusBarHidden: true }}
+            name="Login"
+            component={LoginScreen}
+          />
+          <Stack.Screen
+            options={{
+              headerShown: false,
+              statusBarHidden: true,
+            }}
+            name="Home"
+            component={HomeScreen}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
