55f1197c4d6dbcc45b4a16f9fe7b55ae9489b7dc
abdulkadirerdem <akadir_1256@hotmail.com>
2022-11-23 16:57:38 +0300

    List View with Card Comp Added

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,18 +3,48 @@ import AddTodoModal from "../components/_home/AddTodoModal";
 
 // UI Component
 import { StyleSheet, View } from "react-native";
-import { Text } from "@ui-kitten/components";
+import { List, Text } from "@ui-kitten/components";
 
 // api
 import { getTodos } from "../api/todos";
+import TodoCard from "../components/_home/TodoCard";
 
 const HomeScreen = (props) => {
   const currentlyUser = props.route.params;
+  const [todos, setTodos] = useState([]);
   // add todo
   const [reqIsSuccess, setReqIsSuccess] = useState({});
 
+  useEffect(() => {
+    getTodos().then(async (todos) => {
+      if (currentlyUser.fullAccess) {
+        setTodos(todos);
+      } else {
+        setTodos(
+          todos?.filter(
+            (todo) =>
+              todo.todoOwnerMail === currentlyUser.mail &&
+              todo.todoStatus === true
+          )
+        );
+      }
+    });
+  }, []);
+
   const updateTodos = () => {
-    getTodos();
+    getTodos().then((todos) => {
+      if (currentlyUser.fullAccess) {
+        setTodos(todos);
+      } else {
+        setTodos(
+          todos.filter(
+            (todo) =>
+              todo.todoOwnerMail === currentlyUser.mail &&
+              todo.todoStatus === true
+          )
+        );
+      }
+    });
     setTitle("");
     setDescription("");
     setReqIsSuccess({});
@@ -32,6 +62,14 @@ const HomeScreen = (props) => {
         <Text style={{ fontSize: 20 }}>ToDo App</Text>
         {!currentlyUser?.fullAccess && AddTodoModal(currentlyUser)}
       </View>
+      {todos && (
+        <List
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+          data={todos}
+          renderItem={TodoCard}
+        />
+      )}
     </View>
   );
 };
@@ -41,7 +79,7 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   header: {
     padding: 20,
-    borderBottomWidth: 1,
+    borderBottomWidth: 0.35,
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
