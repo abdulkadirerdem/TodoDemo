fcaefff7a9abb7cd59296c49c0a4fb0250bb3024
abdulkadirerdem <akadir_1256@hotmail.com>
2022-11-23 18:48:29 +0300

    Crud Handle Functions

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -6,7 +6,13 @@ import { StyleSheet, View } from "react-native";
 import { List, Text } from "@ui-kitten/components";
 
 // api
-import { getTodos } from "../api/todos";
+import {
+  getTodos,
+  addTodo,
+  editTodo,
+  activeTodo,
+  inactiveTodo,
+} from "../api/todos";
 import TodoCard from "../components/_home/TodoCard";
 
 const HomeScreen = (props) => {
@@ -45,8 +51,6 @@ const HomeScreen = (props) => {
         );
       }
     });
-    setTitle("");
-    setDescription("");
     setReqIsSuccess({});
   };
 
@@ -56,18 +60,46 @@ const HomeScreen = (props) => {
     }
   }, [reqIsSuccess]);
 
+  const addTodoHandle = (data) => {
+    let { title, description } = data;
+
+    addTodo(title, description, currentlyUser.mail).then((info) =>
+      setReqIsSuccess(info)
+    );
+  };
+
+  const editTodoHandle = (id, data) => {
+    editTodo(id, data).then((info) => setReqIsSuccess(info));
+  };
+
+  const todoStatusHanlde = (type, id, data) => {
+    if (type === "active") {
+      activeTodo(id, data).then((info) => setReqIsSuccess(info));
+    }
+    if (type === "passive") {
+      inactiveTodo(id, data).then((info) => setReqIsSuccess(info));
+    }
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={{ fontSize: 20 }}>ToDo App</Text>
-        {!currentlyUser?.fullAccess && AddTodoModal(currentlyUser)}
+        {!currentlyUser?.fullAccess && AddTodoModal(addTodoHandle)}
       </View>
       {todos && (
         <List
           style={styles.container}
           contentContainerStyle={styles.contentContainer}
           data={todos}
-          renderItem={TodoCard}
+          renderItem={(data) => (
+            <TodoCard
+              info={data}
+              navigate={props.navigation.navigate}
+              editTodoHandle={editTodoHandle}
+              todoStatusHanlde={todoStatusHanlde}
+            />
+          )}
         />
       )}
     </View>
