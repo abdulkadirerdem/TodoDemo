a92427dfb6c74398fd5ef3185ffb274c3e15cf5c
abdulkadirerdem <akadir_1256@hotmail.com>
2022-11-23 15:10:01 +0300

    Home Screen - Header Bar Created

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,14 +1,53 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AddTodoModal from "../components/_home/AddTodoModal";
 
-const HomeScreen = () => {
+// UI Component
+import { StyleSheet, View } from "react-native";
+import { Text } from "@ui-kitten/components";
+
+// api
+import { getTodos } from "../api/todos";
+
+const HomeScreen = (props) => {
+  const currentlyUser = props.route.params;
+  // add todo
+  const [reqIsSuccess, setReqIsSuccess] = useState({});
+
+  const updateTodos = () => {
+    getTodos();
+    setTitle("");
+    setDescription("");
+    setReqIsSuccess({});
+  };
+
+  useEffect(() => {
+    if (reqIsSuccess?.isSuccess) {
+      updateTodos();
+    }
+  }, [reqIsSuccess]);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={{ fontSize: 20 }}>ToDo App</Text>
+        {!currentlyUser?.fullAccess && AddTodoModal(currentlyUser)}
+      </View>
     </View>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    padding: 20,
+    borderBottomWidth: 1,
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  container: {
+    flex: 1,
+  },
+});
